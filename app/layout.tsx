ab2250d37d5fb1ff4f1d59dbeb000164faee3083
Visa Pollari <visa.pollari@gmail.com>
2023-04-22 20:59:58 +0300

    no template

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,11 @@ export default function RootLayout({
       <body className="flex flex-col min-h-screen">
         <SupabaseProvider>
           <Header />
-          {children}
+          <div className="flex-grow min-h-0">
+            <main className="flex flex-col justify-center items-center p-8">
+              {children}
+            </main>
+          </div>
           <Footer />
         </SupabaseProvider>
       </body>
--- app/template.tsx
+++
@@ -1,9 +0,0 @@
-export default function Template({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="flex-grow min-h-0">
-      <main className="flex flex-col justify-center items-center p-8">
-        {children}
-      </main>
-    </div>
-  );
-}
