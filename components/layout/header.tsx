49d70fc9353883c234f1e6f5471616cd60ec9f4d
Visa Pollari <visa.pollari@gmail.com>
2023-04-24 00:36:58 +0300

    fix import

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useAppContext } from '@/context/appConteçxt';
+import { useAppContext } from '@/context/appContext';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
